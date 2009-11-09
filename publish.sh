da29783c69f2522f50bcd35179a2b7080624a283
Tobias Koch <tobias.koch@gmail.com>
2009-11-09 16:18:25 +0100

    Add shell script for compiling all formats in one batch

---
+++ publish.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+	echo "Usage: $0 <revision>"
+	exit 1
+else
+	tag="$1"
+fi
+
+git checkout $tag || {
+	echo "Could not checkout tag '$tag' :("
+	exit 2
+}
+
+mkdir tmp html pdf
+
+(cd html &&
+	ecromedos -fxhtml ../src/manual.xml) > /dev/null
+
+(cd tmp &&
+	ecromedos -fxelatex ../src/manual.xml &&
+	for i in `seq 3`; do
+		xelatex report.tex
+	done &&
+	mv report.pdf ../pdf) > /dev/null
+
+rm -fr tmp
