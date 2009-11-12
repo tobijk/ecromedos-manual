3130894834f64b884270aa8a855f8e9769f48b04
Tobias Koch <tobias.koch@gmail.com>
2009-11-12 14:45:33 +0100

    Add switches to publish.sh to allow producing only a single format.

--- publish.sh
+++ publish.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+output=all
+while true; do
+	case "$1" in
+		--html-only)
+			output=html
+			shift
+		;;
+		--pdf-only)
+			output=pdf
+			shift
+		;;
+		*)
+			break
+		;;
+	esac
+done
+
 if [ -z "$1" ]; then
 	echo "Usage: $0 <revision>"
 	exit 1
@@ -12,16 +29,19 @@ git checkout $tag || {
 	exit 2
 }
 
-mkdir tmp html pdf
+mkdir -p tmp html pdf
 
-(cd html &&
+if [ $output = 'all' ] || [ $output = 'html' ]; then
+	(cd html &&
 	ecromedos -fxhtml ../src/manual.xml) > /dev/null
+fi
 
-(cd tmp &&
+if [ $output = 'all' ] || [ $output = 'pdf' ]; then
+	(cd tmp &&
 	ecromedos -fxelatex ../src/manual.xml &&
 	for i in `seq 3`; do
 		xelatex report.tex
 	done &&
 	mv report.pdf ../pdf/manual.pdf) > /dev/null
-
+fi
 rm -fr tmp
